1c3dffa4a3268afced44ff175fc755e1b6ea3469
Jean Osco <willjean29@gmail.com>
2024-08-26 23:05:59 -0500

    feat(space): add lambda and rest api

--- space-finder/src/infra/launcher.ts
+++ space-finder/src/infra/launcher.ts
@@ -1,5 +1,9 @@
 import { App } from "aws-cdk-lib";
 import { DataStack } from "./stacks/data.stack";
+import { LambdaStack } from "./stacks/lambda.stack";
+import { ApiStack } from "./stacks/api.stack";
 
 const app = new App();
 new DataStack(app, "DataStack");
+const lambdaStack = new LambdaStack(app, "LambdaStack");
+new ApiStack(app, "ApiStack", { helloLambdaIntegration: lambdaStack.helloLambdaIntegration });
---
+++ space-finder/src/infra/stacks/lambda.stack.ts
@@ -0,0 +1,18 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import { Code, Function as LambdaFunction, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import { join } from "path";
+
+export class LambdaStack extends Stack {
+  public readonly helloLambdaIntegration: LambdaIntegration;
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id);
+    const helloLambda = new LambdaFunction(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "hello.main",
+      code: Code.fromAsset(join(__dirname, "../../services")),
+    });
+    this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
+  }
+}
