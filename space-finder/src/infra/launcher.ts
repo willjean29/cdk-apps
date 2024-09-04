9c1d96d8aa8194314bf2bc8113394937f1d08262
Jean Osco <willjean29@gmail.com>
2024-09-03 23:04:16 -0500

    feat(space): create cognito pools

--- space-finder/src/infra/launcher.ts
+++ space-finder/src/infra/launcher.ts
@@ -2,8 +2,10 @@ import { App } from "aws-cdk-lib";
 import { DataStack } from "./stacks/data.stack";
 import { LambdaStack } from "./stacks/lambda.stack";
 import { ApiStack } from "./stacks/api.stack";
+import { AuthStack } from "./stacks/auth.stack";
 
 const app = new App();
 const dataStack = new DataStack(app, "DataStack");
 const lambdaStack = new LambdaStack(app, "LambdaStack", { spacesTable: dataStack.spacesTable });
+new AuthStack(app, "AuthStack");
 new ApiStack(app, "ApiStack", { spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration });
---
+++ space-finder/src/infra/stacks/auth.stack.ts
@@ -0,0 +1,39 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { Construct } from "constructs";
+import { CfnUserPool, UserPool, UserPoolClient } from "aws-cdk-lib/aws-cognito";
+import { CfnOutput, RemovalPolicy } from "aws-cdk-lib/core";
+
+export class AuthStack extends Stack {
+  private userPool: UserPool;
+  private userPoolClient: UserPoolClient;
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+    this.createUserPool();
+    this.createUserPoolClient();
+  }
+
+  private createUserPool() {
+    this.userPool = new UserPool(this, "SpaceUserPool", {
+      selfSignUpEnabled: true,
+      signInAliases: { username: true, email: true },
+    });
+
+    new CfnOutput(this, "SpaceUserPoolId", {
+      value: this.userPool.userPoolId,
+    });
+  }
+  private createUserPoolClient() {
+    this.userPoolClient = this.userPool.addClient("SpaceUserPoolClient", {
+      authFlows: {
+        adminUserPassword: true,
+        userSrp: true,
+        custom: true,
+        userPassword: true,
+      },
+    });
+    new CfnOutput(this, "SpaceUserPoolClientId", {
+      value: this.userPoolClient.userPoolClientId,
+    });
+  }
+}
