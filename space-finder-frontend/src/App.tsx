5512c98e02db93b357cfc010bdfce9e44c401bd3
Jean Osco <willjean29@gmail.com>
2024-09-10 21:02:31 -0500

    feat(space-frontend): add routing

--- space-finder-frontend/src/App.tsx
+++ space-finder-frontend/src/App.tsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import NavBar from "./components/Navbar";
 import "./App.css";
 
 function App() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [userName, setUserName] = useState<string | undefined>(undefined);
+
+  const router = createBrowserRouter([
+    {
+      element: (
+        <>
+          <NavBar userName={userName} />
+          <Outlet />
+        </>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <div>Hello world!</div>,
+        },
+        {
+          path: "/login",
+          element: <div>Login page</div>,
+        },
+        {
+          path: "/profile",
+          element: <div>Profile page</div>,
+        },
+        {
+          path: "/createSpace",
+          element: <div>Create space page</div>,
+        },
+        {
+          path: "/spaces",
+          element: <div>Spaces page </div>,
+        },
+      ],
+    },
+  ]);
   return (
-    <div className="App">
-      <h1>App works!</h1>
+    <div className="wrapper">
+      <RouterProvider router={router} />
     </div>
   );
 }
