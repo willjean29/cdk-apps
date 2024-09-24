50c241f6e5d67e58cfd4be18825171f2571a5048
Jean Osco <willjean29@gmail.com>
2024-09-24 18:29:34 -0500

    feat(space): add sns trigger

--- space-finder/src/infra/stacks/monitor.stack.ts
+++ space-finder/src/infra/stacks/monitor.stack.ts
@@ -1,10 +1,30 @@
 import { Duration, Stack, StackProps } from "aws-cdk-lib";
 import { Alarm, Metric, Unit } from "aws-cdk-lib/aws-cloudwatch";
+import { SnsAction } from "aws-cdk-lib/aws-cloudwatch-actions";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Topic } from "aws-cdk-lib/aws-sns";
+import { LambdaSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
 import { Construct } from "constructs";
+import { join } from "path";
 
 export class MonitorStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const webHookLambda = new NodejsFunction(this, "WebHookLambda", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "handler",
+      entry: join(__dirname, "../../services/monitor/handler.ts"),
+    });
+
+    const alarmTopic = new Topic(this, "AlarmTopic", {
+      displayName: "AlarmTopic",
+      topicName: "AlarmTopic",
+    });
+
+    alarmTopic.addSubscription(new LambdaSubscription(webHookLambda));
+
     const spacesApi4xxAlarm = new Alarm(this, "SpacesApi4xxAlarm", {
       metric: new Metric({
         namespace: "AWS/ApiGateway",
@@ -20,5 +40,8 @@ export class MonitorStack extends Stack {
       threshold: 5,
       alarmName: "SpacesApi4xxAlarm",
     });
+    const topicAction = new SnsAction(alarmTopic);
+    spacesApi4xxAlarm.addAlarmAction(topicAction);
+    spacesApi4xxAlarm.addOkAction(topicAction);
   }
 }
