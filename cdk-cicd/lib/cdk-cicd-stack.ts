7bcdf5919a09cadd681cefef49d038ecbf7500d1
Jean Osco <willjean29@gmail.com>
2024-09-26 18:38:39 -0500

    cicd: add test stage

--- cdk-cicd/lib/cdk-cicd-stack.ts
+++ cdk-cicd/lib/cdk-cicd-stack.ts
@@ -1,11 +1,12 @@
 import * as cdk from "aws-cdk-lib";
 import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
+import { PipelineStage } from "./pipeline.stack";
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    new CodePipeline(this, "AwesomePipeline", {
+    const pipeline = new CodePipeline(this, "AwesomePipeline", {
       pipelineName: "AwesomePipeline",
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub("willjean29/cdk-apps", "cicd-practice"),
@@ -13,5 +14,11 @@ export class CdkCicdStack extends cdk.Stack {
         primaryOutputDirectory: "cdk-cicd/cdk.out",
       }),
     });
+
+    const testStage = pipeline.addStage(
+      new PipelineStage(this, "PipelineTestStage", {
+        stageName: "test",
+      })
+    );
   }
 }
